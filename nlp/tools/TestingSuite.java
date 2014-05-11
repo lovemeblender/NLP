dc315899ab1dc2fec8886e9d0fa9858880e6ac6c
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-05-11 02:27:08 +0100

    Update TestingSuite.java

--- nlp/tools/TestingSuite.java
+++ nlp/tools/TestingSuite.java
@@ -5,7 +5,9 @@ public class TestingSuite {
 	public static void main(String[] args) {
 		
 		NGram bigram = new NGram(); 
-		String[] bigrams = bigram.getNGrams("a ha hou ha", 2);
+		String[] bigrams = bigram.getNGrams("Their only home league defeat so far this season came against Chelsea "
+				+ "on 3 February. Sunderland are the only other side to have taken any points away from the Etihad in the "
+				+ "2013-14 Premier League campaign - they drew 2-2 there last month.", 2);
 	
 		for(String abigram : bigrams) {
 			System.out.println(abigram);
